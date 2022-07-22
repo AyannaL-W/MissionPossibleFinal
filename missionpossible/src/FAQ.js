b3892e50f2fa9fb95c4d5b822b6c53b288266503
AyannaL-W <71052423+AyannaL-W@users.noreply.github.com>
2022-07-22 12:32:26 -0400

    FAQ

--- missionpossible/src/FAQ.js
+++ missionpossible/src/FAQ.js
@@ -4,7 +4,7 @@ import { Button } from 'bootstrap';
 
 const FAQ = (Langue) => {
     
-    const [FAQHeader1, setFAQHeader1] = useState('+ Question 1');
+    const [FAQHeader1, setFAQHeader1] = useState('+ Est-ce que ');
     const [FAQBody1, setFAQBody1] = useState('Reponse 1');
     const [FAQHeader2, setFAQHeader2] = useState('+ Question 2');
     const [FAQBody2, setFAQBody2] = useState('Reponse 2');
@@ -13,25 +13,25 @@ const FAQ = (Langue) => {
     const [FAQHeader4, setFAQHeader4] = useState('+ Question 4');
     const [FAQBody4, setFAQBody4] = useState('Reponse 4');
 
-    const LangueChange = () => {
+    function PageLangue(section){
         if(Langue.LanguageId === "French"){
-            setFAQHeader1('Home');
-            setFAQBody1('Games');
-            setFAQHeader2('Goals');
-            setFAQBody2('Analisis');
-            setFAQHeader3('Step by Step');
-            setFAQBody3('Connexion');
-            setFAQHeader4('English');
-            setFAQBody4('idk');
+            if(section === "Q1") return "Est-ce que j'ai accès à tous les jeux au monde?";
+            else if (section === "R1") return "Malheureusement non, ce n'est pas encore à ce niveau";
+            else if (section === "Q2") return "Est-ce que je peux partager mes scores avec mes amis?";
+            else if (section === "R2") return "Pas encore, mais bientôt!"
+            else if (section === "Q3")   return "Est-ce que vous avez des objectifs pour Crash Bandicoot?";
+            else if(section === "R3")    return "Oui, absolument! Va a la section Objectifs pour les voirs";
+            else if (section === "Q4")   return "Est-ce que vous avez des objectifs pour COD : Vanguard?";
+            else if(section === "R4")    return "Oui, absolument! Va a la section Objectifs pour les voirs";
         } else{
-            setFAQHeader1('sdf');
-            setFAQBody1('sdf');
-            setFAQHeader2('sdf');
-            setFAQBody2('sdf');
-            setFAQHeader3('sdf');
-            setFAQBody3('sdf');
-            setFAQHeader4('sdf');
-            setFAQBody4('sdf');
+            if(section === "Q1") return "Do I have access to every game in the world?";
+            else if (section === "R1") return "Unfortunately not, we are not at that level yet";
+            else if (section === "Q2") return "Can I share my scores with my friends?";
+            else if (section === "R2") return "Not yet, but soon!"
+            else if (section === "Q3")   return "Do you have goals for Crash Bandicoot?";
+            else if(section === "R3")    return "Yes, absolutely! You can go in the Goals section to see them";
+            else if (section === "Q4")   return "Do you have goals for COD : Vanguard?";
+            else if(section === "R4")    return "Yes, absolutely! You can go in the Goals section to see them";
         }
     }
 
@@ -40,20 +40,20 @@ const FAQ = (Langue) => {
             <Accordion>
                 FAQ
                 <Accordion.Item eventKey='1'>
-                    <Accordion.Header>{FAQHeader1}</Accordion.Header>
-                    <Accordion.Body>{FAQBody1}</Accordion.Body>
+                    <Accordion.Header>{PageLangue("Q1")}</Accordion.Header>
+                    <Accordion.Body>{PageLangue("R1")}</Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey='2'>
-                    <Accordion.Header>{FAQHeader2}</Accordion.Header>
-                    <Accordion.Body>{FAQBody2}</Accordion.Body>
+                    <Accordion.Header>{PageLangue("Q2")}</Accordion.Header>
+                    <Accordion.Body>{PageLangue("R2")}</Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey='3'>
-                    <Accordion.Header>{FAQHeader3}</Accordion.Header>
-                    <Accordion.Body>{FAQBody3}</Accordion.Body>
+                    <Accordion.Header>{PageLangue("Q3")}</Accordion.Header>
+                    <Accordion.Body>{PageLangue("R3")}</Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey='4'>
-                    <Accordion.Header>{FAQHeader4}</Accordion.Header>
-                    <Accordion.Body>{FAQBody4}</Accordion.Body>
+                    <Accordion.Header>{PageLangue("Q4")}</Accordion.Header>
+                    <Accordion.Body>{PageLangue("R4")}</Accordion.Body>
                 </Accordion.Item>
             </Accordion>
         </div>
