24a291c958f330a269475107544aceba0543cabf
AyannaL-W <71052423+AyannaL-W@users.noreply.github.com>
2022-07-24 19:07:31 -0400

    deploy try

--- missionpossible/src/index.js
+++ missionpossible/src/index.js
@@ -4,11 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
